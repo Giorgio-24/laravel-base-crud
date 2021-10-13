96799003953e283d1811ea316308840d3cfd241f
Giorgio Andrei <cipuandrei@libero.it>
2021-10-13 17:45:52 +0200

    comics showed in home

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/',  'ComicController@index');
 
 Route::resource('/comics', 'ComicController');
