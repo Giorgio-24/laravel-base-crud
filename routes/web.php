bb47a6c427ffad0d2183aadc21d024a727bdf4c0
Giorgio Andrei <cipuandrei@libero.it>
2021-10-13 15:09:42 +0200

    project resetted

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', 'ComicController@index')->name('home');
-
-Route::get('comics/{id}', 'ComicController@show')->name('comics.show');
+/* Route::get('/') */
