7e952d1296f48e4ed3a544adc972863cbc78ab11
Giorgio Andrei <cipuandrei@libero.it>
2021-10-14 23:24:14 +0200

    modal added to permanently delete button

--- resources/views/comics/trash.blade.php
+++ resources/views/comics/trash.blade.php
@@ -1,7 +1,43 @@
 @extends('layouts.main')
 @section('content')
+
+    {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Launch static backdrop modal
+    </button> --}}
+
+    {{-- <form class="delete-form" method="POST" action="{{ route('comics.perdel', $comic->id) }}">
+        @method('DELETE')
+        @csrf
+        <button class="btn rounded bg-danger text-white" type="submit">Delete</button>
+    </form> --}}
+
+
     <div class="row justify-content-around mt-5">
         @foreach ($comics as $comic)
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop-{{ $comic->id }}" data-bs-backdrop="static"
+                data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">WARNING! THIS ACTION IS IRREVERSIBLE</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Are you sure you want to delete the comic "{{ $comic->title }}" permanently from the database?
+                            You will not be able to restore it again.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <form class="delete-form" method="POST" action="{{ route('comics.perdel', $comic->id) }}">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn rounded bg-danger text-white" type="submit">Delete permanently</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="card pt-3" style="width: 18rem;">
                 <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}-image">
                 <div class="card-body">
@@ -18,11 +54,11 @@
                             </form>
                         </div>
                         <div class="d-inline-block ms-2">
-                            <form method="POST" action="{{ route('comics.perdel', $comic->id) }}">
-                                @method('DELETE')
-                                @csrf
-                                <button class="btn rounded bg-danger text-white" type="submit">Delete</button>
-                            </form>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop-{{ $comic->id }}">
+                                Delete
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -31,7 +67,14 @@
     </div>
 @endsection
 @section('script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <script>
+        /*         const form = document.querySelectorAll('.delete-form');
+                                                                                                                                                    form.addeventListener('submit', (e) => {
+                                                                                                                                                        e.preventDefault();
 
+                                                                                                                                                    }); */
     </script>
 @endsection
